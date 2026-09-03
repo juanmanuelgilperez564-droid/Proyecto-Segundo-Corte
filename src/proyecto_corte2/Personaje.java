d07f9a70382fa4904eff6036ea22aeb3ce61ecc6
Juan Manuel <jgilp@uniempresarial.edu.co>
2026-09-02 20:26:03 -0500

    S3. Atributos y constructores de clase padre

--- src/proyecto_corte2/Personaje.java
+++ src/proyecto_corte2/Personaje.java
@@ -1,4 +1,22 @@
 package proyecto_corte2;
 public class Personaje {
+    //Atributos
+    protected String nombre;
+    protected int nivel;
+    protected double salud;
+    protected boolean esBueno;
     
+    //Constructores
+    public Personaje(){
+        this("Pedro", 1, 100, true);
+    }
+    public Personaje(String nombre){
+        this(nombre, 1, 100, true);
+    } 
+    public Personaje(String nombre, int nivel, double salud, boolean esBueno){
+        this.nombre=nombre;
+        this.nivel=nivel;
+        this.salud=salud;
+        this.esBueno=esBueno;
+    }
 }
