b7defa7c828d64e088f524a79ec2132b6b20ce17
Juan Manuel <jgilp@uniempresarial.edu.co>
2026-09-02 20:44:09 -0500

    S4. Separacion y creacion de paquetes

--- src/proyecto_corte2/Proyecto_Corte2.java
+++ src/proyecto_corte2/Proyecto_Corte2.java
@@ -1,6 +1,14 @@
 package proyecto_corte2;
+/*Importaciones de clases de otro paquete para que no se pueda 
+acceder directamente a los atributos desde el main*/
+
+import Juego.Arquero;
+import Juego.Guerrero;
+import Juego.Heroe;
+import Juego.Personaje;
+
 public class Proyecto_Corte2 {
     public static void main(String[] args) {
-        
+        Personaje p1 = new Personaje();
     }
 }
